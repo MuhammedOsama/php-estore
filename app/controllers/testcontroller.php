a6a54ce41bc9f4e97573cf569143343e172f7191
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2022-10-19 11:07:50 +0200

    PHP eStore

---
+++ app/controllers/testcontroller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PHPMVC\Controllers;
+
+use PHPMVC\Lib\Validate;
+
+class TestController extends AbstractController
+{
+    use Validate;
+
+    public function defaultAction()
+    {
+    }
+}
